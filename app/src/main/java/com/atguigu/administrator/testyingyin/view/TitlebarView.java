e083380085e761c28b2f76f00d89568ffabd7984
saygogo <1183769411@qq.com>
2017-01-06 18:48:07 +0800

    这是321影音

---
+++ app/src/main/java/com/atguigu/administrator/testyingyin/view/TitlebarView.java
@@ -0,0 +1,16 @@
+package com.atguigu.administrator.testyingyin.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by Administrator on 2017/1/6.
+ */
+
+public class TitlebarView extends LinearLayout{
+
+    public TitlebarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
